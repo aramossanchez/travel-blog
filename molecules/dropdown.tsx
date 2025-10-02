88e56641e660242d90dcf340aea02ec5a2d63d3d
aramossanchez <armandoramossanchez@gmail.com>
2025-10-02 18:14:40 +0200

    feat: dropdown wip

--- molecules/dropdown.tsx
+++ molecules/dropdown.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import ChevronRightIcon from "@/atoms/icons/chevronRight";
 import iconSelector from "@/services/iconSelector";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 interface DropdownMoleculeProps {
   text: string;
@@ -14,25 +16,36 @@ export default function DropdownMolecule({
   dropdownKeys,
   icons = false,
 }: DropdownMoleculeProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const iconButtonStyle = isOpen ? "rotate-270" : "rotate-90";
+  const dropdownStyle = isOpen
+    ? "top-[120%] opacity-100 pointer-events-auto max-h-96"
+    : "top-[80%] opacity-0 pointer-events-none max-h-0";
+
   return (
-    <section className="sticky top-0 w-full flex justify-end">
+    <section className="sticky top-0 w-full flex justify-end z-50">
       <div className="flex flex-col items-end gap-y-2 w-fit relative">
-        <div className="bg-foreground text-background px-4 py-2 rounded-md flex flex-row items-center gap-x-10 cursor-pointer">
+        <button
+          className="text-background px-4 py-2 rounded-md flex flex-row items-center gap-x-10 cursor-pointer bg-foreground"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <span>{text}</span>
           <ChevronRightIcon
             size={20}
             color="var(--background)"
-            className="rotate-90"
+            className={`${iconButtonStyle} transition-transform duration-300`}
           />
-        </div>
-        <div className="absolute flex flex-col w-full rounded-md overflow-hidden top-[120%]">
+        </button>
+        <div
+          className={`${dropdownStyle} absolute flex flex-col w-full rounded-md overflow-hidden duration-300 transition-all`}
+        >
           {dropdownKeys.map((key: string) => {
             return (
               <>
                 <Link
                   href={`#${key}`}
                   key={`footer-${key}`}
-                  className="bg-foreground text-background hover:bg-foregroundSecondary px-4 py-2 flex items-center gap-x-4"
+                  className="bg-foreground text-background hover:bg-foregroundSecondary px-4 flex items-center gap-x-4 py-2"
                 >
                   {icons && iconSelector({ section: key, size: 20 })}
                   <span className="capitalize">{key}</span>
